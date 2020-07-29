9da21f117900e445129c1d1c7964bb21b2e2d5d8
tucker <tuckerwray84@gmail.com>
2020-07-28 20:33:18 -0400

    implement class Stack using built-in list methods

--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,59 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+#stack last in first out
+'''
+push the newest value onto the top of the stack 
+like pancakes
+last on top of my plate is first to be eaten
+
+push pancake on top of my stack
+
+pop pancake off top
+
+
+'''
+
 class Stack:
     def __init__(self):
         self.size = 0
         # self.storage = ?
+        self.storage=[]
 
     def __len__(self):
-        pass
+        return len(self.storage)
+        return self.size
 
     def push(self, value):
+        self.storage.append(value)
+        return len(self.storage)  
         pass
 
     def pop(self):
+        self.storage.pop()
+        return len(self.storage)
         pass
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        # self.storage = ?
+        self.storage=[]
+
+    def __len__(self):
+        return f'{len(self.storage)}'
+        #return f'{self.size}'
+
+    def push(self, value):
+        self.storage.append(value)
+        return len(self.storage)  
+        pass
+
+    def pop(self):
+        if len(self.self.storage)>0:
+            return f'{self.storage.pop()}'
+        elif len(self.storage is 0):
+            return f'error Pop top from empty stack'
+
+
